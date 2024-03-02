934a737f623702fb968458504abb7c875301bd0c
naybyal <officiallynabiel@gmail.com>
2024-03-02 16:57:33 +0530

    Add Task

--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,11 @@
 import NewTask from "@/components/NewTask";
-import {Calendar} from "@/components/ui/calendar";
+
 
 export default function Home() {
-    const [date, setDate] = React.useState<Date | undefined>(new Date())
-  return (
-      <>
-          <NewTask />
-          <Calendar
-              mode="single"
-              selected={date}
-              onSelect={setDate}
-              className="rounded-md border"
-          />
-      </>
-  );
+
+    return (
+          <>
+              <NewTask class="m-5"/>
+          </>
+    );
 }
---
+++ src/components/AddTask.jsx
@@ -0,0 +1,42 @@
+import {Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle} from "@/components/ui/card";
+import {Label} from "@/components/ui/label";
+import {Input} from "@/components/ui/input";
+import {Select, SelectContent, SelectItem, SelectTrigger, SelectValue} from "@/components/ui/select";
+import {Button} from "@/components/ui/button";
+
+export default function AddTask() {
+
+    return(
+        <Card className="w-[350px] m-auto border-0">
+            <CardHeader>
+                <CardTitle>New Task</CardTitle>
+                <CardDescription>Add a task to the list</CardDescription>
+            </CardHeader>
+            <CardContent>
+                <form>
+                    <div className="grid w-full items-center gap-5">
+                        <div className="flex flex-col space-y-1.5">
+                            <Label htmlFor="name">Task Title</Label>
+                            <Input id="name" placeholder="Title of the task"/>
+                        </div>
+                        <div className="flex flex-col space-y-1.5">
+                            <Label htmlFor="completion-time">Completion Time</Label>
+                            <Select>
+                                <SelectTrigger id="completion-time">
+                                    <SelectValue placeholder="Select"/>
+                                </SelectTrigger>
+                                <SelectContent position="popper">
+                                    <SelectItem value="today">Today</SelectItem>
+                                    <SelectItem value="tomorrow">Tomorrow</SelectItem>
+                                </SelectContent>
+                            </Select>
+                        </div>
+                    </div>
+                </form>
+            </CardContent>
+            <CardFooter className="grid justify-center align-middle gap-3">
+                <Button>Add to List</Button>
+            </CardFooter>
+        </Card>
+    )
+}

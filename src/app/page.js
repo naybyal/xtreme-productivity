c1b9d6cab044def5eaeeae72100a7f1624f03330
naybyal <officiallynabiel@gmail.com>
2024-03-03 00:45:10 +0530

    Fix : Alignment

--- src/app/page.js
+++ src/app/page.js
@@ -6,9 +6,9 @@ import RainNoise from "@/components/RainNoise";
 export default function Home() {
 
     return (
-          <>
+          <div className="grid grid-flow-col justify-evenly">
               <NewTask />
               <RainNoise class="mt-5 space-y-2" />
-          </>
+          </div>
     );
 }

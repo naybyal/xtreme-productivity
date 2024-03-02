cad25e37c5fe42f991fd1ce4fdfd63a9d5141c20
naybyal <officiallynabiel@gmail.com>
2024-03-02 17:00:31 +0530

    Add Task

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,3 +1,4 @@
+'use client'
 import {Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle} from "@/components/ui/card";
 import {Label} from "@/components/ui/label";
 import {Input} from "@/components/ui/input";
